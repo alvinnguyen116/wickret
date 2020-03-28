9a4d023a1e64d783b1bc258daae4b2b770e2ca5d
alvin <@>
2020-03-28 14:43:28 -0700

    Updates

---
+++ precache-manifest.ef42303c6e4f183dc1f132526445459f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a2aa3c0032777cbd88b4dd22cbc4c3",
+    "url": "/wickret/index.html"
+  },
+  {
+    "revision": "df29a458596b0de1c4dc",
+    "url": "/wickret/static/css/main.8b631615.chunk.css"
+  },
+  {
+    "revision": "35319f4c3e59f7fa3552",
+    "url": "/wickret/static/js/2.c78741ba.chunk.js"
+  },
+  {
+    "revision": "df29a458596b0de1c4dc",
+    "url": "/wickret/static/js/main.52e9c4a8.chunk.js"
+  },
+  {
+    "revision": "fc1e45d88d975dd9a0af",
+    "url": "/wickret/static/js/runtime-main.f14fda36.js"
+  }
+]);
